43267bd80a7fa574caf33b6e318f50dec190e123
Ming Liu <liuming@lmws.net>
2016-08-05 15:34:08 -0700

    initial implementation of HeaderableEtag

--- lib/headerable_etag.rb
+++ lib/headerable_etag.rb
@@ -1,4 +1,35 @@
 require "headerable_etag/version"
+require 'digest/sha2'
 
 module HeaderableEtag
+  def self.headers_string(current_headers, header_keys_for_etag)
+    header_values_for_etag = current_headers.values_at(*header_keys_for_etag)
+    headers_string = header_values_for_etag.sort.join
+    return headers_string
+  end
+
+  def self.etag_with_headers(current_headers, header_keys_for_etag)
+    headers_string_for_etag = headers_string(current_headers, header_keys_for_etag)
+    new_etag = digest(headers_string_for_etag + current_headers['ETag'])
+    return new_etag
+  end
+
+  def self.digest(string)
+    Digest::SHA256.hexdigest(string).byteslice(0,32)
+  end
+
+  class Middleware
+    def initialize(app, header_keys_for_etag=[], options={})
+      @app = app
+      @header_keys_for_etag = header_keys_for_etag
+    end
+
+    def call(env)
+      status, headers, body = @app.call(env)
+      if headers['ETag']
+        headers['ETag'] = HeaderableEtag.etag_with_headers(headers, @header_keys_for_etag)
+      end
+      [status, headers, body]
+    end
+  end
 end
--- spec/headerable_etag_spec.rb
+++ spec/headerable_etag_spec.rb
@@ -5,7 +5,42 @@ describe HeaderableEtag do
     expect(HeaderableEtag::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+
+  describe ".headers_string" do
+    let(:current_headers) { { a: 'one', b: 'two', c: 'three' } }
+    let(:header_keys_for_etag) { [:b, :c] }
+    subject { described_class.headers_string(current_headers, header_keys_for_etag) }
+    it { is_expected.to eq 'threetwo' }
+  end
+
+  describe ".etag_with_headers" do
+    let(:current_headers) { { 'ETag' => 'etag', 'Extra' => 'extra' } }
+    let(:header_keys_for_etag) { ['Extra'] }
+    subject { described_class.etag_with_headers(current_headers, header_keys_for_etag) }
+    before { allow(described_class).to receive(:digest) }
+    it { subject; expect(described_class).to have_received(:digest).with('extraetag') }
+  end
+
+  describe ".digest" do
+    let(:string) { SecureRandom.hex }
+    subject { described_class.digest(string) }
+    it { expect(subject.length).to eq 32 }
+  end
+
+  describe described_class::Middleware do
+    let(:app_call_return) { [200, { 'ETag' => 'etag'}, ''] }
+    let(:app) { double(call: app_call_return) }
+    let(:env) { double }
+    let(:header_keys_for_etag) { ['Extra'] }
+    let(:middleware) { described_class.new(app, header_keys_for_etag) }
+    subject { middleware }
+    it { is_expected.to be_a described_class }
+
+    context "#call" do
+      subject { middleware.call(env) }
+      before { allow(HeaderableEtag).to receive(:etag_with_headers) }
+      it { subject; expect(app).to have_received(:call).with(env) }
+      it { subject; expect(HeaderableEtag).to have_received(:etag_with_headers) }
+    end
   end
 end
